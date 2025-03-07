818c2d23bdc4ad402141107f3de141026224501b
zdemirgithub <zehrademir.gm@gmail.com>
2025-03-07 23:30:11 +0300

    .

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import { CourseCard } from "@/components/CourseCard";
 import { getCourses } from "@/sanity/lib/courses/getCourses";
+import { internalGroqTypeReferenceTo, SanityImageCrop, SanityImageHotspot, Slug } from "@/sanity.types";
 
 export const dynamic = "force-static";
 export const revalidate = 3600; // revalidate at most every hour
 
 export default async function Home() {
+  // Fetch courses
   const courses = await getCourses();
 
   return (
@@ -23,7 +25,67 @@ export default async function Home() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-16">
-          {courses.map((course) => (
+          {courses.map((course: {
+            _id: string;
+            _type: "course";
+            _createdAt: string;
+            _updatedAt: string;
+            _rev: string;
+            title?: string;
+            price?: number;
+            slug: string | null;
+            description?: string;
+            image?: {
+              asset?: {
+                _ref: string;
+                _type: "reference";
+                _weak?: boolean;
+                [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+              };
+              hotspot?: SanityImageHotspot;
+              crop?: SanityImageCrop;
+              _type: "image";
+            };
+            category: {
+              _id: string;
+              _type: "category";
+              _createdAt: string;
+              _updatedAt: string;
+              _rev: string;
+              name?: string;
+              slug?: Slug;
+              description?: string;
+              icon?: string;
+              color?: string;
+            } | null;
+            modules?: Array<{
+              _ref: string;
+              _type: "reference";
+              _weak?: boolean;
+              _key: string;
+              [internalGroqTypeReferenceTo]?: "module";
+            }>;
+            instructor: {
+              _id: string;
+              _type: "instructor";
+              _createdAt: string;
+              _updatedAt: string;
+              _rev: string;
+              name?: string;
+              bio?: string;
+              photo?: {
+                asset?: {
+                  _ref: string;
+                  _type: "reference";
+                  _weak?: boolean;
+                  [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+                };
+                hotspot?: SanityImageHotspot;
+                crop?: SanityImageCrop;
+                _type: "image";
+              };
+            } | null;
+          }) => (
             <CourseCard
               key={course._id}
               course={course}
