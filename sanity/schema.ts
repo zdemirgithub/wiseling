49d740f756f79113b1f46a7cad615d9b5eecb540
zdemirgithub <zehrademir.gm@gmail.com>
2025-03-08 00:59:32 +0300

    .

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,14 +1,12 @@
 import { type SchemaTypeDefinition } from "sanity";
-import {
-  courseType,
-  moduleType,
-  lessonType,
-  instructorType,
-  studentType,
-  enrollmentType,
-  categoryType,
-  lessonCompletionType,
-} from "./schemaTypes";
+import { courseType } from "./schemaTypes/courseType";
+import { moduleType } from "./schemaTypes/moduleType";
+import { lessonType } from "./schemaTypes/lessonType";
+import { instructorType } from "./schemaTypes/instructorType";
+import { studentType } from "./schemaTypes/studentType";
+import { enrollmentType } from "./schemaTypes/enrollmentType";
+import { categoryType } from "./schemaTypes/categoryType";
+import { lessonCompletionType } from "./schemaTypes/lessonCompletionType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
