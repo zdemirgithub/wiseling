d4abdb286da5e77ce843e0821139a1e40b3831d3
zdemirgithub <zehrademir.gm@gmail.com>
2025-03-07 23:43:08 +0300

    .

--- sanity/lib/courses/searchCourses.ts
+++ sanity/lib/courses/searchCourses.ts
@@ -7,10 +7,63 @@ export async function searchCourses(term: string) {
     description match $term + "*" ||
     category->name match $term + "*"
   )] {
-    ...,
+    _id,
+    _type,
+    _createdAt,
+    _updatedAt,
+    _rev,
+    title,
+    price,
+    description,
     "slug": slug.current,
-    "category": category->{...},
-    "instructor": instructor->{...}
+    image {
+      asset->{
+        _ref,
+        _type,
+        _weak
+      },
+      hotspot,
+      crop,
+      _type
+    },
+    "category": category->{
+      _id,
+      _type,
+      _createdAt,
+      _updatedAt,
+      _rev,
+      name,
+      slug,
+      description,
+      icon,
+      color
+    },
+    "instructor": instructor->{
+      _id,
+      _type,
+      _createdAt,
+      _updatedAt,
+      _rev,
+      name,
+      bio,
+      photo {
+        asset->{
+          _ref,
+          _type,
+          _weak
+        },
+        hotspot,
+        crop,
+        _type
+      }
+    },
+    modules[]->{
+      _ref,
+      _type,
+      _weak,
+      _key,
+      [internalGroqTypeReferenceTo]
+    }
   }`);
 
   const result = await sanityFetch({
